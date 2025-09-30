c8f29dc093e779a919219647154f3068d8ca2e56
shnd <sahand.javid@gmail.com>
2025-09-29 22:23:47 -0700

    new config path

--- generators/app/constants.js
+++ generators/app/constants.js
@@ -2,7 +2,7 @@ import os from 'os'
 import path from 'path' 
 
 export const GENERATOR_NAME = 'try-typescript'
-export const ABSOLUTE_CONFIG_FILENAME = path.join(os.homedir(), `.yo-rc.${GENERATOR_NAME}.json`)
+export const ABSOLUTE_CONFIG_FILENAME = path.join(os.homedir(), '.try', `${GENERATOR_NAME}.yo-rc.json`)
 
 export const $PACKAGE_DIRECTORY = '$PACKAGE_DIRECTORY'
 export const $PACKAGE_INDEX_FILE = '$PACKAGE_INDEX_FILE'
